0aab2dce7c1644acf013b7ff99e86a295ca14ffb
kyle <kylepwarren@gmail.com>
2024-04-16 10:36:48 -0400

    mostly rewrite, because when actually used, found lots of exceptions. unit tests should help

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::{cmp, env, fs, process};
+use std::{ env, fs, process};
 
-
-fn show_usage(){
-
-    println!(r#"
+fn show_usage() {
+    println!(
+        r#"
     
     usage: overlapmerge <file1> <file2>
     
@@ -13,116 +12,172 @@ fn show_usage(){
 
     if no overlap is found, then nothing is output
 
-    "#);
-
+    "#
+    );
 }
 
+fn overlap_merge_two_string(
+    shorter_string: &str,
+    longer_string: &str,
+    prefix: &str,
+    suffix: &str,
+) -> String {
+    let shorter_string_max_idx = shorter_string.char_indices().count();
+    let longer_string_max_idx = longer_string.char_indices().count();
 
-fn overlap_merge_two_string(file_1_data_str: &str, file_2_data_str: &str) -> String {
+    let prefix_max_idx = prefix.char_indices().count();
+    let suffix_max_idx = suffix.char_indices().count();
 
-    //dbg!(file_1_data_str);
-    //dbg!(file_2_data_str);
-
-    if file_1_data_str.eq(file_2_data_str) {
-        return format!("{}",file_1_data_str);
-        //return Ok(None);
-        //process::exit(0);
+    if shorter_string_max_idx > longer_string_max_idx {
+        return overlap_merge_two_string(longer_string, shorter_string, prefix, suffix);
     }
-    
 
-    let file_1_data_str_max_idx = file_1_data_str.char_indices().count();
-    let file_2_data_str_max_idx = file_2_data_str.char_indices().count();
+    if shorter_string_max_idx == longer_string_max_idx && shorter_string.eq(longer_string) {
+        return format!("{}", longer_string);
+    } else if suffix_max_idx == 0 && longer_string.starts_with(shorter_string) {
+        return format!("{}{}", prefix, longer_string);
+    } else if prefix_max_idx == 0 && longer_string.ends_with(shorter_string) {
+        return format!("{}{}", longer_string, suffix);
+    } else if prefix_max_idx == 0 && suffix_max_idx == 0 && longer_string.contains(shorter_string) {
+        return format!("{}", longer_string);
+    }
 
-    let min_str_len = cmp::min(file_1_data_str_max_idx, file_2_data_str_max_idx);
-
-    //dbg!(min_str_len);
-
-    for cur_len in (1..min_str_len).rev(){
-        //dbg!(cur_len);
+    if shorter_string_max_idx > 1 {
         
-        let (f1_sl_idx, _) = file_1_data_str.char_indices().nth(cur_len).unwrap();
-        let (f2_sl_idx, _) = file_2_data_str.char_indices().nth(cur_len).unwrap();
+        
+        let mut prefix_recursion_result = "".to_string();
+        let mut suffix_recursion_result = "".to_string();
 
-        let f1_slice = &file_1_data_str[..f1_sl_idx];
-        let f2_slice = &file_2_data_str[..f2_sl_idx];
+        if prefix_max_idx == 0 {
+            // there is not a prefix, so it's safe to try suffix recursion 
+            let (short_string_suffix_slice_idx, _) = shorter_string
+                .char_indices()
+                .nth(shorter_string_max_idx - 1)
+                .unwrap();
+            let short_string_suffix_slice = &shorter_string[short_string_suffix_slice_idx..];
+            let short_string_suffix_remainder_slice = &shorter_string[..short_string_suffix_slice_idx];
 
-        if file_2_data_str.ends_with(f1_slice){
-            //dbg!(f1_slice);
-            return format!("{}{}",file_2_data_str,&file_1_data_str[f1_sl_idx..]);
-            //process::exit(0);
-        }
-        else if file_1_data_str.ends_with(f2_slice) {
-            //dbg!(f2_slice);
-            return format!("{}{}",file_1_data_str,&file_2_data_str[f2_sl_idx..]);
-            //process::exit(0);
-        }
-        else if file_2_data_str.starts_with(f1_slice){
-            //dbg!(f1_slice);
-            return format!("{}",file_2_data_str);
-            //process::exit(0);
-        }
-        else if file_1_data_str.starts_with(f2_slice) {
-            //dbg!(f2_slice);
-            return format!("{}",file_1_data_str);
-            //process::exit(0);
+            suffix_recursion_result = overlap_merge_two_string(
+                short_string_suffix_remainder_slice,
+                longer_string,
+                "",
+                short_string_suffix_slice,
+            );
         }
 
-    } 
+        if suffix_max_idx == 0 {
+            // there is not a suffix, so it's safe to try prefix recursion 
+            let (short_string_prefix_slice_idx, _) = shorter_string.char_indices().nth(1).unwrap();
+        
+            let short_string_prefix_slice = &shorter_string[..short_string_prefix_slice_idx];
+            let short_string_prefix_remainder_slice = &shorter_string[short_string_prefix_slice_idx..];    
+
+            prefix_recursion_result = overlap_merge_two_string(
+                short_string_prefix_remainder_slice,
+                longer_string,
+                short_string_prefix_slice,
+                "",
+            );
+        }
+
+        let prefix_recursion_result_str = prefix_recursion_result.as_str();
+        let suffix_recursion_result_str = suffix_recursion_result.as_str();
+
+        let prefix_recursion_result_char_count = prefix_recursion_result_str.char_indices().count();
+        let suffix_recursion_result_char_count = suffix_recursion_result_str.char_indices().count();
+
+        if prefix_recursion_result_char_count == 0 {
+            return suffix_recursion_result;
+        }
+        else if suffix_recursion_result_char_count == 0 {
+            return prefix_recursion_result;
+        }
+        else if prefix_recursion_result_char_count < suffix_recursion_result_char_count {
+            return prefix_recursion_result;
+        }
+        else{
+            return suffix_recursion_result;
+        }
+    }
 
     return "".to_string();
-
 }
 
-
 fn main() {
-
-    if env::args().len() != 3 
-    {
+    if env::args().len() != 3 {
         show_usage();
         process::exit(1);
     }
 
     let args: Vec<String> = env::args().collect();
-    
+
     let file_1_path = &args[1];
     let file_2_path = &args[2];
-    
-    //dbg!(file_1_path);
-    //dbg!(file_2_path);
 
-    let file_1_data = fs::read_to_string(file_1_path).expect(&format!("Unable to read file: {}", file_1_path));
-    let file_2_data = fs::read_to_string(file_2_path).expect(&format!("Unable to read file: {}", file_2_path));
+    let file_1_data =
+        fs::read_to_string(file_1_path).expect(&format!("Unable to read file: {}", file_1_path));
+    let file_2_data =
+        fs::read_to_string(file_2_path).expect(&format!("Unable to read file: {}", file_2_path));
 
-    let file_1_data_str =  file_1_data.as_str();
-    let file_2_data_str =  file_2_data.as_str();
+    let file_1_data_str = file_1_data.as_str();
+    let file_2_data_str = file_2_data.as_str();
 
-    let om_res = overlap_merge_two_string(file_1_data_str,file_2_data_str);
+    let om_res = overlap_merge_two_string(file_1_data_str, file_2_data_str, "", "");
 
-    println!("{}",om_res);
+    println!("{}", om_res);
     process::exit(0);
 
-
     //println!("{}{}",file_1_data_str,file_2_data_str);
-
 }
 
 #[test]
-fn test_merge(){
+fn test_merge_a() {
+    let str_a = r#"abc"#;
+    let str_b = r#"abcde"#;
 
-    let str_a = r#"a
-b
-c"#;
-
-let str_b = r#"a
-b
-c
-d
-e"#;
-
-let om_res = overlap_merge_two_string(&str_a, &str_b);
-
-
-assert_eq!(str_b,om_res);
-
-
+    let om_res = overlap_merge_two_string(&str_a, &str_b, "", "");
+    let om_res_str = om_res.as_str();
+    assert_eq!(om_res_str,str_b);
 }
+
+#[test]
+fn test_merge_b() {
+    let str_a = r#"abc"#;
+    let str_b = r#"abcde"#;
+
+    let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
+    let om_res_str = om_res.as_str();
+    assert_eq!(om_res_str,str_b);
+}
+
+#[test]
+fn test_merge_c() {
+    let str_a = r#"abcx"#;
+    let str_b = r#"abcde"#;
+
+    let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
+    let om_res_str = om_res.as_str();
+    assert_eq!(om_res_str,"");
+}
+
+#[test]
+fn test_merge_d() {
+    let str_a = r#"xabc"#;
+    let str_b = r#"abcde"#;
+
+    let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
+    let om_res_str = om_res.as_str();
+    assert_eq!(om_res_str,"xabcde");
+}
+
+#[test]
+fn test_merge_e() {
+    let str_a = r#"🙂abc"#;
+    let str_b = r#"abcde"#;
+
+    let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
+    let om_res_str = om_res.as_str();
+    assert_eq!(om_res_str,"🙂abcde");
+}
+
+
