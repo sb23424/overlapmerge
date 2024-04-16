8d60b65ae6e41bb265c6964e96f2dcce3035073e
kyle <kylepwarren@gmail.com>
2024-04-16 12:01:49 -0400

    abandoning recursion, because stack overflow

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{ env, fs, process};
+use std::{env, fs, process};
 
 fn show_usage() {
     println!(
@@ -16,6 +16,37 @@ fn show_usage() {
     );
 }
 
+fn check_for_immediate_overlap(
+    shorter_string: &str,
+    longer_string: &str,
+    prefix: &str,
+    suffix: &str,
+) -> Option<String> {
+    let shorter_string_max_idx = shorter_string.char_indices().count();
+    let longer_string_max_idx = longer_string.char_indices().count();
+
+    let prefix_max_idx = prefix.char_indices().count();
+    let suffix_max_idx = suffix.char_indices().count();
+
+    if shorter_string_max_idx > longer_string_max_idx {
+        return check_for_immediate_overlap(longer_string, shorter_string, prefix, suffix);
+    }
+
+    let mut res_string_option: Option<String> = None;
+
+    if shorter_string_max_idx == longer_string_max_idx && shorter_string.eq(longer_string) {
+        res_string_option = Some(format!("{}", longer_string));
+    } else if suffix_max_idx == 0 && longer_string.starts_with(shorter_string) {
+        res_string_option = Some(format!("{}{}", prefix, longer_string));
+    } else if prefix_max_idx == 0 && longer_string.ends_with(shorter_string) {
+        res_string_option = Some(format!("{}{}", longer_string, suffix));
+    } else if prefix_max_idx == 0 && suffix_max_idx == 0 && longer_string.contains(shorter_string) {
+        res_string_option = Some(format!("{}", longer_string));
+    }
+
+    return res_string_option;
+}
+
 fn overlap_merge_two_string(
     shorter_string: &str,
     longer_string: &str,
@@ -25,82 +56,77 @@ fn overlap_merge_two_string(
     let shorter_string_max_idx = shorter_string.char_indices().count();
     let longer_string_max_idx = longer_string.char_indices().count();
 
-    let prefix_max_idx = prefix.char_indices().count();
-    let suffix_max_idx = suffix.char_indices().count();
-
     if shorter_string_max_idx > longer_string_max_idx {
         return overlap_merge_two_string(longer_string, shorter_string, prefix, suffix);
     }
 
-    if shorter_string_max_idx == longer_string_max_idx && shorter_string.eq(longer_string) {
-        return format!("{}", longer_string);
-    } else if suffix_max_idx == 0 && longer_string.starts_with(shorter_string) {
-        return format!("{}{}", prefix, longer_string);
-    } else if prefix_max_idx == 0 && longer_string.ends_with(shorter_string) {
-        return format!("{}{}", longer_string, suffix);
-    } else if prefix_max_idx == 0 && suffix_max_idx == 0 && longer_string.contains(shorter_string) {
-        return format!("{}", longer_string);
+    let mut best_match = "".to_string();
+
+    let max_iter_idx = shorter_string_max_idx - 1;
+
+    for short_idx in 0..max_iter_idx {
+        //prefix splitter
+        let (short_string_prefix_slice_idx, _) =
+            shorter_string.char_indices().nth(short_idx).unwrap();
+        let short_string_prefix_slice = &shorter_string[..short_string_prefix_slice_idx];
+        let short_string_prefix_remainder_slice = &shorter_string[short_string_prefix_slice_idx..];
+
+        //suffix splitter
+        let (short_string_suffix_slice_idx, _) = shorter_string
+            .char_indices()
+            .nth(max_iter_idx - short_idx)
+            .unwrap();
+        let short_string_suffix_slice = &shorter_string[short_string_suffix_slice_idx..];
+        let short_string_suffix_remainder_slice = &shorter_string[..short_string_suffix_slice_idx];
+
+        let prefix_immediate_overlap = check_for_immediate_overlap(
+            short_string_prefix_remainder_slice,
+            longer_string,
+            short_string_prefix_slice,
+            "",
+        );
+
+        let suffix_immediate_overlap = check_for_immediate_overlap(
+            short_string_suffix_remainder_slice,
+            longer_string,
+            "",
+            short_string_suffix_slice,
+        );
+
+        if prefix_immediate_overlap.is_some() && suffix_immediate_overlap.is_some() {
+            let prefix_immediate_overlap_string = prefix_immediate_overlap.unwrap();
+            let suffix_immediate_overlap_string = suffix_immediate_overlap.unwrap();
+
+            let prefix_immediate_char_count =
+                prefix_immediate_overlap_string.char_indices().count();
+            let suffix_immediate_char_count =
+                suffix_immediate_overlap_string.char_indices().count();
+
+            if prefix_immediate_char_count == suffix_immediate_char_count {
+                best_match = prefix_immediate_overlap_string;
+                break;
+            } else if prefix_immediate_char_count < suffix_immediate_char_count {
+                best_match = prefix_immediate_overlap_string;
+                break;
+            } else {
+                best_match = suffix_immediate_overlap_string;
+                break;
+            }
+        } else if prefix_immediate_overlap.is_some() {
+            let prefix_immediate_overlap_string = prefix_immediate_overlap.unwrap();
+            best_match = prefix_immediate_overlap_string;
+            break;
+        } else if suffix_immediate_overlap.is_some() {
+            let suffix_immediate_overlap_string = suffix_immediate_overlap.unwrap();
+            best_match = suffix_immediate_overlap_string;
+            break;
+        }
+
+        println!("{}", short_idx);
     }
 
-    if shorter_string_max_idx > 1 {
-        
-        
-        let mut prefix_recursion_result = "".to_string();
-        let mut suffix_recursion_result = "".to_string();
-
-        if prefix_max_idx == 0 {
-            // there is not a prefix, so it's safe to try suffix recursion 
-            let (short_string_suffix_slice_idx, _) = shorter_string
-                .char_indices()
-                .nth(shorter_string_max_idx - 1)
-                .unwrap();
-            let short_string_suffix_slice = &shorter_string[short_string_suffix_slice_idx..];
-            let short_string_suffix_remainder_slice = &shorter_string[..short_string_suffix_slice_idx];
-
-            suffix_recursion_result = overlap_merge_two_string(
-                short_string_suffix_remainder_slice,
-                longer_string,
-                "",
-                short_string_suffix_slice,
-            );
-        }
-
-        if suffix_max_idx == 0 {
-            // there is not a suffix, so it's safe to try prefix recursion 
-            let (short_string_prefix_slice_idx, _) = shorter_string.char_indices().nth(1).unwrap();
-        
-            let short_string_prefix_slice = &shorter_string[..short_string_prefix_slice_idx];
-            let short_string_prefix_remainder_slice = &shorter_string[short_string_prefix_slice_idx..];    
-
-            prefix_recursion_result = overlap_merge_two_string(
-                short_string_prefix_remainder_slice,
-                longer_string,
-                short_string_prefix_slice,
-                "",
-            );
-        }
-
-        let prefix_recursion_result_str = prefix_recursion_result.as_str();
-        let suffix_recursion_result_str = suffix_recursion_result.as_str();
-
-        let prefix_recursion_result_char_count = prefix_recursion_result_str.char_indices().count();
-        let suffix_recursion_result_char_count = suffix_recursion_result_str.char_indices().count();
-
-        if prefix_recursion_result_char_count == 0 {
-            return suffix_recursion_result;
-        }
-        else if suffix_recursion_result_char_count == 0 {
-            return prefix_recursion_result;
-        }
-        else if prefix_recursion_result_char_count < suffix_recursion_result_char_count {
-            return prefix_recursion_result;
-        }
-        else{
-            return suffix_recursion_result;
-        }
-    }
-
-    return "".to_string();
+    return best_match;
+   
 }
 
 fn main() {
@@ -127,7 +153,6 @@ fn main() {
     println!("{}", om_res);
     process::exit(0);
 
-    //println!("{}{}",file_1_data_str,file_2_data_str);
 }
 
 #[test]
@@ -137,7 +162,7 @@ fn test_merge_a() {
 
     let om_res = overlap_merge_two_string(&str_a, &str_b, "", "");
     let om_res_str = om_res.as_str();
-    assert_eq!(om_res_str,str_b);
+    assert_eq!(om_res_str, str_b);
 }
 
 #[test]
@@ -147,7 +172,7 @@ fn test_merge_b() {
 
     let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
     let om_res_str = om_res.as_str();
-    assert_eq!(om_res_str,str_b);
+    assert_eq!(om_res_str, str_b);
 }
 
 #[test]
@@ -157,7 +182,7 @@ fn test_merge_c() {
 
     let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
     let om_res_str = om_res.as_str();
-    assert_eq!(om_res_str,"");
+    assert_eq!(om_res_str, "");
 }
 
 #[test]
@@ -167,7 +192,7 @@ fn test_merge_d() {
 
     let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
     let om_res_str = om_res.as_str();
-    assert_eq!(om_res_str,"xabcde");
+    assert_eq!(om_res_str, "xabcde");
 }
 
 #[test]
@@ -177,7 +202,15 @@ fn test_merge_e() {
 
     let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
     let om_res_str = om_res.as_str();
-    assert_eq!(om_res_str,"🙂abcde");
+    assert_eq!(om_res_str, "🙂abcde");
 }
 
+#[test]
+fn test_merge_f() {
+    let str_a = r#"cdef"#;
+    let str_b = r#"abcde"#;
 
+    let om_res = overlap_merge_two_string(&str_b, &str_a, "", "");
+    let om_res_str = om_res.as_str();
+    assert_eq!(om_res_str, "abcdef");
+}
